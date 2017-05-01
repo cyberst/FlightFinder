614b50369b2564c2e700b763aef1d6775560e457
Ali Rıza Salihoğlu <salihoglual@itu.edu.tr>
2017-05-01 19:11:25 +0200

    Fix query declaration

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var Movie = require("./models/flight");
 
 var app = express();
 
-var query;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
 
+var query;
 
 // Server index page
 app.get("/", function (req, res) {
